98b2547e40f87306f2e97de430f0397b61963130
Igor Moraes <igor.sgm@gmail.com>
2022-05-07 11:41:07 -0700

    GhostTest implemented

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -53,6 +53,7 @@ class TestCase extends \Orchestra\Testbench\TestCase
         $app['config']->set('ghost', [
             'ghost_api_version' => 4,
             'admin_domain' => 'https://demo.ghost.io',
+            'admin_key' => '1234:56789',
             'content_key' => '22444f78447824223cefc48062',
             'debug_enabled' => true,
             'cache' => [
