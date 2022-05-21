86afb2a830239cf6b3b1c7e11e67b55f6d71842f
Igor Moraes <igor.sgm@gmail.com>
2022-05-20 18:59:48 -0700

    Offers docs added

--- src/Models/Resources/Offer.php
+++ src/Models/Resources/Offer.php
@@ -12,44 +12,98 @@ class Offer implements ResourceInterface
     public $id;
 
     /**
+     * Internal name for an offer, must be unique
+     *
      * @var string|null
      */
     public $name;
 
     /**
+     * Shortcode for the offer, for example: https://yoursite.com/black-friday
+     *
      * @var string|null
      */
     public $code;
 
     /**
+     * Name displayed in the offer window
+     *
      * @var string|null
      */
     public $displayTitle;
 
     /**
+     * Text displayed in the offer window
+     *
      * @var boolean
      */
     public $displayDescription;
 
     /**
+     * Percent or fixed - whether the amount off is a percentage or fixed
      * @var string|null
      */
     public $type;
 
     /**
+     * month or year - denotes if offer applies to tier's monthly or yearly price
      * @var string|null
      */
     public $cadence;
 
     /**
+     * Offer discount amount, as a percentage or fixed value as set in type.
+     * Amount is always denoted by the smallest currency unit (e.g., 100 cents instead of $1.00 in USD)
      * @var string|null
      */
     public $amount;
 
     /**
-     * @var array
+     * once/forever/repeating. repeating duration is only available when cadence is month
+     *
+     * @var string|null
      */
     public $duration;
+
+    /**
+     * Number of months offer should be repeated when duration is repeating
+     *
+     * @var string|null
+     */
+    public $durationInMonths;
+
+    /**
+     * Denotes whether the offer `currency` is restricted. If so, changing the currency invalidates the offer
+     * @var string|null
+     */
+    public $currencyRestriction;
+
+    /**
+     * Fixed type offers only - specifies tier's currency as three letter ISO currency code
+     *
+     * @var string|null
+     */
+    public $currency;
+
+    /**
+     * Active or archived - denotes if the offer is active or archived
+     *
+     * @var mixed|null
+     */
+    public $status;
+
+    /**
+     * Number of times the offer has been redeemed
+     *
+     * @var mixed|null
+     */
+    public $redemptionCount;
+
+    /**
+     * Tier on which offer is applied
+     * @var Tier|null
+     */
+    public ?Tier $tier;
 
     /**
      * @var string|null
@@ -60,36 +114,6 @@ class Offer implements ResourceInterface
      * @var string|null
      */
     public $updatedAt;
-
-    /**
-     * @var string|null
-     */
-    public $durationInMonths;
-
-    /**
-     * @var string|null
-     */
-    public $currencyRestriction;
-
-    /**
-     * @var string|null
-     */
-    public $currency;
-
-    /**
-     * @var mixed|null
-     */
-    public $status;
-
-    /**
-     * @var mixed|null
-     */
-    public $redemptionCount;
-
-    /**
-     * @var Tier|null
-     */
-    public ?Tier $tier;
 
     /**
      * @param  array  $array
